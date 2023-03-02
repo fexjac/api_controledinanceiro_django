dab860488cf2bc76530710f87261caeef321ab7c
F3xJ4C <fexjac@@gmail.com>
2023-03-02 19:14:19 -0400

    Atualização para repetição das inserções

--- myproject/financeiro/views.py
+++ myproject/financeiro/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import TransacaoSerializer, UserSerializer
 from .models import Transacao, User
-
+import datetime
 class TransacaoList(generics.ListCreateAPIView):
     queryset = Transacao.objects.all()
     serializer_class = TransacaoSerializer
@@ -18,3 +18,15 @@ class UserList(generics.ListCreateAPIView):
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        repeticoes = serializer.validated_data.get('repeticoes')
+        recorrente = serializer.validated_data.get('recorrente')
+        if recorrente and repeticoes is not None and repeticoes > 1:
+            transacao = serializer.save()
+            for i in range(repeticoes - 1):
+                transacao.pk = None
+                transacao.data = transacao.data + datetime.timedelta(days=7)
+                transacao.save()
+        else:
+            serializer.save()
