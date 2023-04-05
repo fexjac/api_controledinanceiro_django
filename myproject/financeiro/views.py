581383defe8d6f90261acc8e3130d5223332e998
F3xJ4C <fexjac@@gmail.com>
2023-04-05 19:48:27 -0400

    Refatorada a classe de view TransacaoCreateAPIView

--- myproject/financeiro/views.py
+++ myproject/financeiro/views.py
@@ -61,27 +61,19 @@ class TransacaoCreateAPIView(APIView):
     serializer_class = TransacaoSerializer
     def post(self, request, format=None):
         num_transacoes = request.data.get('num_transacoes', None)
-        data_req = request.data.get('data', None)
-        descricao_req = request.data.get('descricao', None)
-        valor_req = request.data.get('valor', None)
-        tipo_req = request.data.get('tipo', None)
-        usuario_req = request.data.get('usuario', None)
 
         if not num_transacoes:
             return Response({'message': 'Por favor, forneça o número de transações.'}, status=status.HTTP_400_BAD_REQUEST)
 
+        serializer = self.serializer_class(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
         transacoes = []
         for i in range(num_transacoes):
-            transacao = Transacao(
-                data = data_req,
-                descricao= descricao_req,
-                valor=valor_req,
-                tipo=tipo_req
-            )
+            transacao = serializer.save()
             transacoes.append(transacao)
 
-        Transacao.objects.bulk_create(transacoes)
-
-        serializer = TransacaoSerializer(transacoes, many=True)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(self.serializer_class(transacoes, many=True).data, status=status.HTTP_201_CREATED)
 
