9bc465523e211d3ad0267744e40a0e909e7aa719
F3xJ4C <fexjac@@gmail.com>
2023-04-07 16:29:13 -0400

    Adicionadas funções de verificação de datas.

--- myproject/financeiro/datatrat.py
+++ myproject/financeiro/datatrat.py
@@ -1,7 +1,10 @@
-from datetime import datetime
+from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
 def trata_data(data):
     data_obj = datetime.strptime(data, '%Y-%m-%d')
+
+    while verifica_fds() is True
+        data_obj = decrementa_dia(data_obj)
 
     return data_obj.strftime('%Y-%m-%d')
 
@@ -10,3 +13,17 @@ def incrementa_mes(data):
     nova_data_obj = data_obj + relativedelta(months=+1)
 
     return nova_data_obj.strftime('%Y-%m-%d')
+
+def verifica_fds(data):
+    data_obj = datetime.strptime(data, '%Y-%m-%d')
+    dv = data.weekday()
+
+    return True if dv in (5, 6) else False
+
+def decrementa_dia(data):
+    data_obj = datetime.strptime(data, '%Y-%m-%d')
+    nova_data_obj = data_obj + relativedelta(days=-1)
+
+    return nova_data_obj.strftime('%Y-%m-%d')
+
+
