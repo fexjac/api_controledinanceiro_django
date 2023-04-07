6f6bd49419eebbcb080c4ebe8b5a861008daf8fa
F3xJ4C <fexjac@@gmail.com>
2023-04-07 16:59:59 -0400

    Fnção de verificação de fds funcionando

--- myproject/financeiro/datatrat.py
+++ myproject/financeiro/datatrat.py
@@ -3,8 +3,9 @@ from dateutil.relativedelta import relativedelta
 def trata_data(data):
     data_obj = datetime.strptime(data, '%Y-%m-%d')
 
-    while verifica_fds() is True:
-        data_obj = decrementa_dia(data_obj)
+    while verifica_fds(data_obj) is True:
+        data_ver = decrementa_dia(data_obj)
+        data_obj = datetime.strptime(data_ver, '%Y-%m-%d')
 
     return data_obj.strftime('%Y-%m-%d')
 
@@ -15,13 +16,23 @@ def incrementa_mes(data):
     return nova_data_obj.strftime('%Y-%m-%d')
 
 def verifica_fds(data):
-    data_obj = datetime.strptime(data, '%Y-%m-%d')
-    dv = data.weekday()
+    if isinstance(data, datetime):
+        data_str = data.strftime('%Y-%m-%d')
+    else:
+        data_str = data
+
+    data_obj = datetime.strptime(data_str, '%Y-%m-%d')
+    dv = data_obj.weekday()
 
     return True if dv in (5, 6) else False
 
 def decrementa_dia(data):
-    data_obj = datetime.strptime(data, '%Y-%m-%d')
+    if isinstance(data, datetime):
+        data_str = data.strftime('%Y-%m-%d')
+    else:
+        data_str = data
+
+    data_obj = datetime.strptime(data_str, '%Y-%m-%d')
     nova_data_obj = data_obj + relativedelta(days=-1)
 
     return nova_data_obj.strftime('%Y-%m-%d')
